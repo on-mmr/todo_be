29288e8f3ea07d5d89c067e9df16380e91875211
on-mmr <59788075+on-mmr@users.noreply.github.com>
2023-06-08 13:48:38 +0900

    add endpoint (#1)
    Co-authored-by: Replit user <>

---
+++ src/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+  return {"message": "Hello World"}
+
+
+uvicorn.run(app, host="0.0.0.0", port="8080")
